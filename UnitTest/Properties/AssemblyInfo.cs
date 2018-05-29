35bc6864376af0f25ceb631da35493a2983fb722
Lewwwwwka <lewwwwwka@gmail.com>
2018-05-29 22:56:32 +0300

    + Микро тесты

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("844e8eba-d2eb-4470-99d0-977d374d3d84")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+            //arrange
+            string str = "5+5";
+            double expected = 10;
+            //act
+            int actual = /*метод из кода*/(str);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+         public void TestMethod1()
+        { 
+            //arrange
+            string str = "5*5";
+            double expected = 25;
+            //act
+            int actual = /*метод из кода*/(str);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+         public void TestMethod1()
+        { 
+            //arrange
+            string str = "5/5";
+            double expected = 1;
+            //act
+            int actual = /*метод из кода*/(str);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+         public void TestMethod1()
+        { 
+            //arrange
+            string str = "5-5";
+            double expected = 0;
+            //act
+            int actual = /*метод из кода*/(str);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
